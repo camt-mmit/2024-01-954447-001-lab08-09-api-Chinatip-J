7e573b4d0ff10624b50c82ffab12a24895776de2
ChinatipJ <chinatipjearsakul@gmail.com>
2025-03-27 14:26:07 +0700

    week8-9

--- src/app/gl/routes.ts
+++ src/app/gl/routes.ts
@@ -10,6 +10,7 @@ import { ContactsService } from './services/contacts.service';
 import { EventService } from './services/event.service';
 import { KeyValueLocalStorageService } from './services/key-value-local-storage.service';
 import { provideOauth } from './services/oauth.service';
+import { isDevMode } from '@angular/core';
 
 export default [
   {
@@ -23,7 +24,10 @@ export default [
         clientSecret: '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',
         authorizationCodeUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
         accessTokenUrl: 'https://oauth2.googleapis.com/token',
-        redirectUri: 'http://localhost:4200/google/authorization',
+        redirectUri:
+          isDevMode() ?
+            'http://localhost:4200/google/authorization '
+          : 'https://camt-mmit.github.io/2024-01-954447-001-lab08-09-api-Chinatip-J/google/authorization ',
       }),
     ],
     children: [
@@ -43,10 +47,10 @@ export default [
           },
           {
             path: 'contacts',
-            providers: [ContactsService], 
+            providers: [ContactsService],
             children: [
-              { path: '', component: GlContactsListComponent }, 
-              { path: 'create', component: GlCreateContactsFormComponent }, 
+              { path: '', component: GlContactsListComponent },
+              { path: 'create', component: GlCreateContactsFormComponent },
             ],
           },
         ],
